2893263c8b7da2ef93149168b605bae34cfa57be
jose <hernandezjos1121@gmail.com>
2023-08-18 14:33:52 -0400

    tenth push

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef} from "react";
 import { SearchNav } from "../Components/Global/SearchNav";
 import { LeftBar } from "../Components/Global/LeftBar";
 import axios from "axios";
 import { MdReplay10, MdForward10 } from "react-icons/md";
-import { AiFillPlayCircle ,AiFillPauseCircle} from "react-icons/ai";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { useParams } from "react-router-dom";
 
 export const Book = () => {
@@ -11,6 +11,9 @@ export const Book = () => {
   const [audioSrc, setAudioSrc] = useState("");
   const [isPlaying, setIsPlaying] = useState(false);
   const [bookData, setBookData] = useState({});
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const audioRef = useRef(null)
   async function fetchBookID() {
     try {
       const response = await axios.get(
@@ -26,8 +29,45 @@ export const Book = () => {
   useEffect(() => {
     fetchBookID();
   }, []);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+
+    if (audioElement) {
+      audioElement.addEventListener("canplaythrough", handleAudioLoad);
+      audioElement.addEventListener("timeupdate", handleTimeUpdate);
+      audioElement.addEventListener("durationchange", handleDurationChange);
+
+      return () => {
+        audioElement.removeEventListener("canplaythrough", handleAudioLoad);
+        audioElement.removeEventListener("timeupdate", handleTimeUpdate);
+        audioElement.removeEventListener(
+          "durationchange",
+          handleDurationChange
+        );
+      };
+    }
+  }, []);
   const togglePlay = () => {
     setIsPlaying(!isPlaying);
+  };
+  const handleAudioLoad = () => {
+    setCurrentTime(audioRef.current.currentTime);
+    setDuration(audioRef.current.duration);
+  };
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef.current.currentTime);
+  };
+
+  const handleDurationChange = () => {
+    setDuration(audioRef.current.duration);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
   };
   return (
     <>
@@ -56,21 +96,36 @@ export const Book = () => {
           </div>
           <div className="audio-btns">
             <div className="audio-btns-wrapper">
-              <button onClick={() => console.log("rewind button clicked")} className="audio-btn-control">
+              <button
+                onClick={() => console.log("rewind button clicked")}
+                className="audio-btn-control"
+              >
                 <MdReplay10 />
               </button>
-              <button onClick={togglePlay} className="audio-btn-control audio-btn-play">
-              {isPlaying ? <AiFillPauseCircle/> : <AiFillPlayCircle />}
+              <button
+                onClick={togglePlay}
+                className="audio-btn-control audio-btn-play"
+              >
+                {isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />}
               </button>
-              <button onClick={() => console.log("forward bytton clicked")} className="audio-btn-control">
+              <button
+                onClick={() => console.log("forward bytton clicked")}
+                className="audio-btn-control"
+              >
                 <MdForward10 />
               </button>
             </div>
           </div>
           <div className="audio-length">
-            <div className="audio-length-time">00:00</div>
-            <input type="range" className="audio-length-duration" />
-            <div className="audio-length-time">3:34</div>
+            <div className="audio-length-time">{formatTime(currentTime)}</div>
+            <input
+          type="range"
+          className="audio-length-duration"
+          value={currentTime}
+          max={duration}
+          onChange={(e) => (audioRef.current.currentTime = e.target.value)}
+        />
+            <div className="audio-length-time">{formatTime(duration)}</div>
           </div>
         </div>
       </div>
